c1cb895c5f0beaf9086379631649f1186bd1e68a
Menash1906 <Menashyt@gmail.com>
2023-04-04 13:05:12 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# EstimatedTimePicker
+
+A description of this package.
---
+++ Tests/EstimatedTimePickerTests/EstimatedTimePickerTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import EstimatedTimePicker
+
+final class EstimatedTimePickerTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(EstimatedTimePicker().text, "Hello, World!")
+    }
+}
